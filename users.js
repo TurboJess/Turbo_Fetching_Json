51b88fe174b5a1b42df96c38aa6aab3ddb0f524a
Intern Jess <internjess@Michele-Muses-Computer.local>
2014-02-07 12:03:46 -0500

    created click event for dummy Data, Now I am trying to do it with jQuery's .Ajax

--- users.js
+++ users.js
@@ -1,33 +1,63 @@
 $(document).ready(function(){
+	$('#see-users-button').click(function(){	
+		$.getJSON('userData.json',function(data){
+			var user=[];
+			$.each(data,function(key,val){
+				// key will be the id and the val/value will be visable
+				user.push('<li id="' + key+ '">' +val+'</li>');
+			})
+			// pass in a tag you havent used you can configure a tag and append
+			// it to th ebody
+			$('<ul/>',{
+				'class':'userList',
+				// what join will do wil take each element in the array
+				// and concatinate them together 
+				// sidebar: if you wanted to seperate the items with a comma put a comma instead of just 
+				// passing an empty string
+				html:user.join("")
 
-	$.getJSON('http://54.213.92.70:8080/getaccount?account=all',function(data){
-	
-	
-		var user={
-				"name": "he",
-				"turbo_user_id": "a",
-				"account_created":"jan 23",
-				"total_matches":0
-			}
-			console.log(user)
+			}).appendTo("body");
+		})
+		console.log(user)
 	})
+});
+	
+	
+		// var user={
+		// 		"name": "he",
+		// 		"turbo_user_id": "a",
+		// 		"account_created":"jan 23",
+		// 		"total_matches":0
+		// 	}
+		// 	console.log(user)
+	// })
 
-	console.log("hello1")
+	// console.log("hello1")
+	// // $.ajax({
+ //    type: 'GET',
+ //    url : 'userDate.js',
+ //    // data : yourData,
+ //    dataType : 'json',
+ //    success : function( results ) {
+ //        console.log('success');
+//  //    }
+// });
 
 	// $.ajax({
-	// 	"url":"54.213.92.70:8080/getaccount?account=2",
+	// 	"url":"userDate.js",
 	// 	"dataType":"jsonp",
-	// 	"data":{
-	// 		"account": "all"
-	// 	},
+	// 	// "data":{
+	// 	// 	"account": "all"
+	// 	// },
 	// 	"type":"GET",
 	// 	success:function(data){
 
-	// 		console.log("hello")
+	// 		console.log(user)
+	// 	}
 
 		
 			
-		});
+	// });
 		// console.log(user)
 		// 	$.each(data,function(key,val){
 		// 		users.push('<li id>')
